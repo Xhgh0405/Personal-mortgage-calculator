9172b954d5b3b276fd62a5143b8fbe3e81e7c789
Xhgh0405 <a0922696660@gmail.com>
2026-04-07 23:13:05 +0800

    edit code

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -2,39 +2,96 @@
 {
     partial class Form1
     {
-        /// <summary>
-        /// 設計工具所需的變數。
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// 清除任何使用中的資源。
-        /// </summary>
-        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        // ===== 控制項宣告（你缺的就在這🔥）=====
+        private System.Windows.Forms.TextBox txtHousePrice;
+        private System.Windows.Forms.TextBox txtDownRate;
+        private System.Windows.Forms.TextBox txtInterest;
+        private System.Windows.Forms.TextBox txtYears;
+        private System.Windows.Forms.TextBox txtGrace;
+
+        private System.Windows.Forms.Label lblLoan;
+        private System.Windows.Forms.Label lblMonthly;
+        private System.Windows.Forms.Label lblTotalInt;
+        private System.Windows.Forms.Label lblTotalPay;
+
+        private System.Windows.Forms.Button btnCalc;
+
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
-            {
                 components.Dispose();
-            }
+
             base.Dispose(disposing);
         }
 
-        #region Windows Form 設計工具產生的程式碼
-
-        /// <summary>
-        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
-        /// 這個方法的內容。
-        /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
-        }
+            this.SuspendLayout();
 
-        #endregion
+            // ===== Form =====
+            this.ClientSize = new System.Drawing.Size(550, 520);
+            this.BackColor = System.Drawing.Color.FromArgb(245, 247, 250);
+            this.Text = "房貸試算器";
+
+            // ===== TextBox =====
+            txtHousePrice = new System.Windows.Forms.TextBox();
+            txtDownRate = new System.Windows.Forms.TextBox();
+            txtInterest = new System.Windows.Forms.TextBox();
+            txtYears = new System.Windows.Forms.TextBox();
+            txtGrace = new System.Windows.Forms.TextBox();
+
+            txtHousePrice.Location = new System.Drawing.Point(200, 100);
+            txtDownRate.Location = new System.Drawing.Point(200, 130);
+            txtInterest.Location = new System.Drawing.Point(200, 160);
+            txtYears.Location = new System.Drawing.Point(200, 190);
+            txtGrace.Location = new System.Drawing.Point(200, 220);
+
+            txtHousePrice.Text = "10000000";
+            txtDownRate.Text = "20";
+            txtInterest.Text = "2";
+            txtYears.Text = "30";
+            txtGrace.Text = "0";
+
+            // ===== Button =====
+            btnCalc = new System.Windows.Forms.Button();
+            btnCalc.Text = "開始計算";
+            btnCalc.Location = new System.Drawing.Point(200, 260);
+            btnCalc.Size = new System.Drawing.Size(120, 40);
+            btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+
+            // ===== Labels（結果）=====
+            lblLoan = new System.Windows.Forms.Label();
+            lblMonthly = new System.Windows.Forms.Label();
+            lblTotalInt = new System.Windows.Forms.Label();
+            lblTotalPay = new System.Windows.Forms.Label();
+
+            lblLoan.Location = new System.Drawing.Point(200, 320);
+            lblMonthly.Location = new System.Drawing.Point(200, 350);
+            lblTotalInt.Location = new System.Drawing.Point(200, 380);
+            lblTotalPay.Location = new System.Drawing.Point(200, 410);
+
+            lblLoan.Text = "0";
+            lblMonthly.Text = "0";
+            lblTotalInt.Text = "0";
+            lblTotalPay.Text = "0";
+
+            // ===== 加入控制項 =====
+            this.Controls.Add(txtHousePrice);
+            this.Controls.Add(txtDownRate);
+            this.Controls.Add(txtInterest);
+            this.Controls.Add(txtYears);
+            this.Controls.Add(txtGrace);
+
+            this.Controls.Add(btnCalc);
+
+            this.Controls.Add(lblLoan);
+            this.Controls.Add(lblMonthly);
+            this.Controls.Add(lblTotalInt);
+            this.Controls.Add(lblTotalPay);
+
+            this.ResumeLayout(false);
+        }
     }
 }
-
--- Form1.cs
+++ Form1.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace 個人房貸試算器
@@ -15,6 +8,69 @@ namespace 個人房貸試算器
         public Form1()
         {
             InitializeComponent();
+        }
+
+        // ===== 計算按鈕 =====
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            // ===== 輸入驗證（老師會看）=====
+            if (!double.TryParse(txtHousePrice.Text, out double house) ||
+                !double.TryParse(txtDownRate.Text, out double downRatePercent) ||
+                !double.TryParse(txtInterest.Text, out double ratePercent) ||
+                !int.TryParse(txtYears.Text, out int years) ||
+                !int.TryParse(txtGrace.Text, out int graceYears))
+            {
+                MessageBox.Show("請輸入正確數值！", "錯誤", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // ===== 轉換 =====
+            double downRate = downRatePercent / 100.0;
+            double annualRate = ratePercent / 100.0;
+
+            double loan = house * (1 - downRate);
+            double monthlyRate = annualRate / 12;
+            int totalMonths = years * 12;
+            int graceMonths = graceYears * 12;
+
+            double monthlyPayment = 0;
+            double totalInterest = 0;
+            double totalPayment = 0;
+
+            // ===== 有寬限期（加分點🔥）=====
+            if (graceMonths > 0)
+            {
+                // 寬限期：只繳利息
+                double interestOnly = loan * monthlyRate;
+
+                // 剩餘期數
+                int remainMonths = totalMonths - graceMonths;
+
+                double normalPayment = loan *
+                    (monthlyRate * Math.Pow(1 + monthlyRate, remainMonths)) /
+                    (Math.Pow(1 + monthlyRate, remainMonths) - 1);
+
+                monthlyPayment = normalPayment;
+
+                totalPayment = (interestOnly * graceMonths) + (normalPayment * remainMonths);
+                totalInterest = totalPayment - loan;
+            }
+            else
+            {
+                // 一般本息攤還
+                monthlyPayment = loan *
+                    (monthlyRate * Math.Pow(1 + monthlyRate, totalMonths)) /
+                    (Math.Pow(1 + monthlyRate, totalMonths) - 1);
+
+                totalPayment = monthlyPayment * totalMonths;
+                totalInterest = totalPayment - loan;
+            }
+
+            // ===== 顯示（格式化）=====
+            lblLoan.Text = loan.ToString("N2");
+            lblMonthly.Text = monthlyPayment.ToString("N2");
+            lblTotalInt.Text = totalInterest.ToString("N2");
+            lblTotalPay.Text = totalPayment.ToString("N2");
         }
     }
 }
